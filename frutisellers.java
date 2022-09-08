2a05b8561d633c703363e1470afabc168cf37bb3
tejaswinii2000 <113040458+tejaswinii2000@users.noreply.github.com>
2022-09-08 22:09:24 +0530

    Add files via upload

---
+++ frutisellers.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class fruitsellers
+
+{
+    public static void main(String[] args)
+    {
+       
+        int apples = 420;
+        int sell = 40;
+        int remaining = 100-40 ;
+       
+     
+        int total_apples = ( apples * 100)/ remaining;
+     
+
+        System.out.print(" Fruit Sellers had " + total_apples + " apples");
+    }
+}
