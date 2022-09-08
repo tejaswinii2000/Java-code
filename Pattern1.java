789785ddbeb1a0035b633a91e11f07ba470241b9
tejaswinii2000 <113040458+tejaswinii2000@users.noreply.github.com>
2022-09-08 18:38:25 +0530

    Add files via upload

---
+++ Pattern1.java
@@ -0,0 +1,33 @@
+imort java.util.Scanner;
+class Pattern1
+{
+	public static void main(string[]args)
+	{
+		int i,j,k,n;
+		for( i= 0; i<3-1;  i++)
+		{
+	        for( j=0; j<i; j++)
+	 	    { 
+			System,out.print(" ");
+		    }
+			for( k=i;  k<=3-1;  k++)
+			{
+			  System.out.print("*"+" ");
+			   }
+		System.out.Println("");
+	}  
+
+	for (i= 3-1;  i>= 0; i__)
+	  {
+		for (j=0; j<i; j++)
+	      { 
+			System.out.print("*"+" ");
+		}
+		  for(k=i, k<=3-1; k++)
+		  	{
+   				System.out.print("*"+" ");
+		  	}
+			System.out.println("");
+		}
+	}
+}
