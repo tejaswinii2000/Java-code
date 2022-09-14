87a4b08784ce524483930f028da83f3ada9c2b47
tejaswinii2000 <113040458+tejaswinii2000@users.noreply.github.com>
2022-09-14 17:59:54 +0530

    Add files via upload

---
+++ GretestLowest.java
@@ -0,0 +1,21 @@
+//to store the gretest and lowest number
+class GretestLowest 
+{
+ 
+	public static void main(String[] args) {
+ 
+		float num1 = 4.25f;
+		int num2 =5;
+ 
+		System.out.println("The largest number of the two numbers is " + Math.max(num1,num2));
+		System.out.println("The smallest number of the two numbers is " + Math.min(num1,num2));
+	}
+}
+
+/*Output
+The largest number of the two numbers is 5.0
+The smallest number of the two numbers is 4.25
+			
+			
+ 
+ 
