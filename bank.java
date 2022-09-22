f2705954b437827a15767de8b46f0d06d94d3393
tejaswinii2000 <113040458+tejaswinii2000@users.noreply.github.com>
2022-09-22 18:26:21 +0530

    Add files via upload

---
+++ bank.java
@@ -0,0 +1,21 @@
+
+class Bank{
+	public static void main (String args[]){
+    Indian_Bank ind = new Indian_Bank();
+    ind.Indian_Bank();
+    SBI_bank sbi = new SBI_bank();
+    sbi.SBI_bank();
+    } 
+}
+class SBI_bank extends Bank{
+    SBI_bank(){
+       System.out.println("You have to wait for others to get in line");
+   }
+
+}
+class Indian_Bank extends Bank {
+    Indian_Bank(){
+        System.out.println("Your account creation will be done in a few minutes");  
+}
+
+ } 
