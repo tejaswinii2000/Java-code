d9e2b5f77492a7d1b4fc70a6605a98f26d392893
tejaswinii2000 <113040458+tejaswinii2000@users.noreply.github.com>
2022-09-26 18:20:22 +0530

    Add files via upload

---
+++ Footballer.java
@@ -0,0 +1,31 @@
+
+public class Footballer {
+	void stadium()
+	{
+
+		System.out.println();
+
+	}
+
+	void fees()
+	{
+		System.out.println("Accourding to country trainer");
+
+	}
+
+	void jersey() 
+	{
+
+		System.out.println("Jersey are blue color with Number");
+
+
+
+   }
+	void trainer()
+	{
+		System.out.println("teach Goal and Defence ");
+	}
+
+}
+
+}
