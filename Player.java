b806c5125c67f4769cf72f66845e72083c4681db
tejaswinii2000 <113040458+tejaswinii2000@users.noreply.github.com>
2022-09-26 18:19:09 +0530

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,26 @@
+
+public class Player{
+	void stadium()
+	{
+
+		System.out.println("small ,clean and neat stadium");
+
+}
+
+	void fees()
+	{
+		System.out.println("Accourding on trainer");
+
+	}
+
+	void jersey() 
+	{
+
+		System.out.println("Jersey are red in  color with Number");
+
+}
+	void trainer()
+	{
+		System.out.println("teach Bating and Balling ");
+	}
+}
